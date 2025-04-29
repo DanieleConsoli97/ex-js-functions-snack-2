8fc5bc7b4de3be246ed598dbe0aee6a1559d045f
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-04-29 11:47:47 +0200

    first commit

---
+++ Snack1.js
@@ -0,0 +1,18 @@
+function somma (num1,num2){
+num1 = parseInt(num1)
+num1 = parseInt(num1)
+return num1 + num2
+} 
+
+console.log(somma(2,3))
+
+
+const sommaDiNumeri = function (num1,num2){
+    num1=parseInt(num1)
+    num2=parseInt(num2)
+    return num1+num2
+}
+console.log(sommaDiNumeri(5,6))
+
+const sommaArrow = (num1,num2) => parseInt(num1) + parseInt(num2)
+console.log(sommaArrow(7,8))
